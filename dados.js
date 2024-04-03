f0df9a3212456a27171a26cd6be8b9a2188c12a6
YasmimSilva0202 <152650731+YasmimSilva0202@users.noreply.github.com>
2024-04-02 21:43:13 -0300

    AtividadeFacebook
    Segue acima a minha atividade Facebook, realizada pela aluna Yasmim Silva 3º ano "c".

---
+++ dados.js
@@ -0,0 +1,6 @@
+user={
+    'name': "Ricky Montgomery ",
+    'photo':https://i.pinimg.com/736x/18/52/49/185249c6fcd1508fcaeb909a340bccca.jpg,
+    'pai': "Luiz Maciel",
+    'mae': " Maria Almeida",
+}
---
+++ teste.js
@@ -0,0 +1,27 @@
+localFoto = document.getElementById('foto');
+localFoto.src = user.photo;
+
+localNome = document.getElementById('nome');
+localNome.innerHTML = user.nome;
+
+localInformação = document.getElementById('informação');
+localInformação.innerHTML = user. informação;
+
+localsexo = document.getElementById('sexo');
+localsexo.innerHTML = user. sexo;
+
+localDataDeNascimento = document.getElementById('DataDeNascimento');
+localDataDeNascimento.innerHTML = user. DataDeNascimento;
+
+localpais = document. getElementById('pais');
+localpais.innerHTML = user. pais;
+
+localirmãos  = document. getElementById('irmãos');
+localirmãos.innerHTML = user. irmãos;
+
+localcidade = document. getElementById('cidade');
+localcidade.innerHTML = user. cidade;
+
+localfrasefavorita = document. getElementById('frasefavoritadomeliante');
+localfrasefavorita.innerHTML = user. frasefavorita;
+    
